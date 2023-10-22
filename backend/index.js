4464e4aee46120052d12f65ff4fb0ae783c332ab
Evgeniia Vdovichenko <e.vdovichenkova@gmail.com>
2023-10-22 22:43:42 +0200

    new data structure

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ let sql;
 //used write and serving our ATOM file
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const { v4: uuidv4 } = require('uuid');
 
 //used to read outside ATOM files, and patse into JSON to be added to our database
 var parseString = xml2js.parseString;
@@ -28,6 +29,10 @@ app.use(express.json());
 const server = app.listen("4000", () => {
   console.log("App is listening at http://localhost:4000");
 });
+
+const serverPath = "./atomfile/atom.xml";
+const publicPath = "https://gioele.uber.space/k/fdla2023/feed1.atom";
+
 
 //Connect to local database with SQLite
 let db = new sqlite3.Database("./db/event.db", (err) => {
@@ -48,46 +53,62 @@ app.get("/events", (req, res) => {
 
     //create the information to be placed in the ATOM file
     let feed = new Feed({
-      title: "Events",
-      author: {
-        name: "Andy Klenzman and Company",
-      },
+      title: "Events feed",
+      id: "urn:uuid:" + uuidv4(), // Generate a random UUID for feed id
+      updated: new Date().toISOString(), //Set the current date as updated
+      link: "https://gioele.uber.space/k/fdla2023/feed1.atom", // Set the link
     });
 
     //Add each database entry to that row
     rows.forEach((row) => {
       feed.addItem({
         title: row.eventTitle,
-        id: row.id,
-        date: row.date,
-        description: row.description,
-        author: { name: row.name },
+        link: row.link, //Set the event link
+        id: row.id, //Use the existing id from the database
+        date: row.date, //Set the date of event
+        published: row.published, // Set publishing date
+        updated: row.updated, // Set updateding date
+        summary: row.description, // Set the description from the row
+        author: [{ name: row.name }], //Set the author name
+        
       });
     });
 
     //Write to the file, and if the write is successful, send it
-    fs.writeFile("./atomfile/atom.xml", feed.atom1(), (err) => {
+    fs.writeFile(serverPath, feed.atom1(), (err) => {
       if (err) console.log(err);
       else {
         console.log("File written successfully\n");
-        res.sendFile(path.join(__dirname, "./atomfile/atom.xml"));
+        fs.copyFile(serverPath, publicPath, (err) => {
+          if (err) throw err;
+          console.log("File copied to public directory");
+        });
+        res.sendFile(serverPath);
       }
     });
   });
-});
+});     
+
 
 //Post a new value into DB
 app.post("/events", (req, res) => {
   const data = req.body;
   const id = `urn:uuid:${uuidv4()}`; //Generate unique id
-  sql = `INSERT INTO events(name, eventTitle, date, description) VALUES (?,?,?,?)`;
+  const publishedDate = new Date().toISOString(); //Set the current date as published
+  const updatedDate = new Date().toISOString(); //Set the current date as updated
+  sql = `INSERT INTO events(name, eventTitle, description, date, link, id, published, updated) 
+        VALUES (?,?,?,?,?,?,?)`;
   db.run(
     sql,
     [
       req.body.yourName,
       req.body.eventTitle,
-      req.body.date,
       req.body.description,
+      req.body.date,
+      "http://example.org/foobar",
+      id,
+      publishedDate,
+      updatedDate,
     ],
     (err) => {
       if (err) return console.error(err.message);
@@ -112,8 +133,12 @@ app.get("/otherEvents", (req, res) => {
             name: entry.author[0].name[0],
             eventTitle: entry.title[0],
             date: entry.published[0],
-            description: entry.summary[0]
-        };
+            description: entry.summary[0],
+            id: entry.id[0], 
+            link: "http://example.org/foobar",
+            published: new Date().toISOString(), 
+            updated: new Date().toISOString() 
+          };
         
         //Assuming the id is provided in the external data
         const id = entry.id[0]; 
@@ -122,8 +147,18 @@ app.get("/otherEvents", (req, res) => {
         const checkSql = `SELECT COUNT(*) FROM events WHERE id = ?`;
         db.get(checkSql, [id], (err, row) => {
           if (row['COUNT(*)'] == 0) { // If the event doesn't exist, insert it
-            const sql = `INSERT INTO events(id, name, eventTitle, date, description) VALUES (?,?,?,?,?)`;
-            db.run(sql, [id, eventData.name, eventData.eventTitle, eventData.date, eventData.description], (err) => {
+            const sql = `INSERT INTO events(id, name, eventTitle, date, description, link, published, updated) 
+                        VALUES (?,?,?,?,?,?,?,?)`;
+            db.run(sql, [
+              id,
+              eventData.name,
+              eventData.eventTitle,
+              eventData.date,
+              eventData.description,
+              "http://example.org/foobar",
+              new Date().toISOString(), 
+              new Date().toISOString() 
+            ], (err) => {
               if (err) return console.error(err.message);
             });
           }
